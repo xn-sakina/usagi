5927168418258d10998ed4cba03728d6409862c2
fz6m <59400654+fz6m@users.noreply.github.com>
2023-10-19 20:55:39 +0800

    chore: basename for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Home } from './pages'
 import { Routes, BrowserRouter, Route } from 'react-router-dom'
 
+const isDev = process.env.NODE_ENV === 'development'
+
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={isDev ? '/' : '/usagi'}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
